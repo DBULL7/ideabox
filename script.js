facb1c357d53d4a1deeb5ce2718e40acb2fb3ffc
kamos1 <keji.amos@gmail.com>
2017-02-14 08:00:15 -0700

    added localstorage and push to array

--- script.js
+++ script.js
@@ -3,38 +3,47 @@ $('.save-button').on('click', function(){
   generateIdea();
 })
 
-
 function generateIdea (){
-  var userTitle = $('.user-title');
-  var userBody = $('.user-body');
-  var cardArray = []
-  new Card(userTitle.value, userBody.value);
+  var $userTitle = $('.user-title');
+  var $userBody = $('.user-body');
+  var cards = new Card($userTitle.val(), $userBody.val());
   console.log("that's an unoriginal idea");
-  console.log(cardArray.title)
+  var cardArray = [];
+  cardArray.push(cards);
+  cardArray.forEach(function(input, index, array){
+    console.log({
+      currentValue: input,
+      currentIndex: index,
+      fullArray: array
+    });
+  });
+  for (var i = 0; i < cardArray.length; i++){
+    var storage = JSON.stringify(cardArray);
+    localStorage.setItem('cardStorage', storage);
+    console.log(cards);
+    console.log(cardArray[i]);
+  }
 }
 
-
-function Card (title, body, quality) {
-  // var userTitle = $('.user-title');
-  // var userBody = $('.user-body');
-  this.title = title;
-  this.body = body;
+function Card ($title, $body, $id, $quality) {
+  this.title = $title;
+  this.body = $body;
+  this.id = $id;
+  this.quality = $quality;
   $('.idea-list').prepend(this.createCardHtml());
 }
 
 Card.prototype.createCardHtml = function(){
-  var userTitle = $('.user-title');
-  var userBody = $('.user-body');
   return `<article class="idea-card">
       <span>
-        <h2>${userTitle.val()}</h2>
+        <h2>${this.title}</h2>
         <button type="button" class="button delete-button"></button>
       </span>
-      <p>${userBody.val()}</p>
+      <p>${this.body}</p>
       <button type="button" class="button upvote-button"></button>
       <button type="button" class="button downvote-button"></button>
       <span>
-        <p>quality: swill </p>
+        <p>quality: ${this.quality} </p>
       </span>
     </article>`
 }
