3271b1460f3a376110944f703d798ce1f05a66e3
Devon Bull <dbull@live.com>
2017-02-13 20:05:21 -0700

    Made it work, needs major refactoring

--- script.js
+++ script.js
@@ -5,30 +5,36 @@ $('.save-button').on('click', function(){
 
 
 function generateIdea (){
-  new Card($('.user-title').val(), $('.user-body').val());
+  var userTitle = $('.user-title');
+  var userBody = $('.user-body');
+  var cardArray = []
+  new Card(userTitle.value, userBody.value);
   console.log("that's an unoriginal idea");
+  console.log(cardArray.title)
 }
 
 
-function Card ($title, body) {
-  this.title = $title;
+function Card (title, body, quality) {
+  // var userTitle = $('.user-title');
+  // var userBody = $('.user-body');
+  this.title = title;
   this.body = body;
   $('.idea-list').prepend(this.createCardHtml());
 }
 
 Card.prototype.createCardHtml = function(){
-  console.log(`<article class="idea-card">
+  var userTitle = $('.user-title');
+  var userBody = $('.user-body');
+  return `<article class="idea-card">
       <span>
-        <h2>${title}</h2>
+        <h2>${userTitle.val()}</h2>
         <button type="button" class="button delete-button"></button>
       </span>
-      <p>${body}</p>
+      <p>${userBody.val()}</p>
       <button type="button" class="button upvote-button"></button>
       <button type="button" class="button downvote-button"></button>
       <span>
-        <p>quality: swill</p>
+        <p>quality: swill </p>
       </span>
     </article>`
-  );
-
 }
