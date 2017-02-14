26bacdff38f32e69110cad385b5611bb12c21802
kamos1 <keji.amos@gmail.com>
2017-02-14 12:05:31 -0700

    Load a card on refresh

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ $('.save-button').on('click', function(){
   clearInputs();
 })
 
+getData();
+
 function Card (title, body) {
   this.title = title;
   this.body = body;
@@ -13,28 +15,35 @@ function Card (title, body) {
   this.id = Date.now();
 }
 
-function createCardHtml(idea) {
+function createCardHtml(cardData) {
   $('.idea-list').prepend(
-    `<article class="idea-card" id="${idea.id}">
+    `<article class="idea-card" id="${cardData.id}">
       <span>
-        <h2>${idea.title}</h2>
+        <h2>${cardData.title}</h2>
         <button type="button" class="button delete-button"></button>
       </span>
-      <p>${idea.body}</p>
+      <p>${cardData.body}</p>
       <button type="button" class="button upvote-button"></button>
       <button type="button" class="button downvote-button"></button>
       <span>
-        <p>${idea.quality}</p>
+        <p>${cardData.quality}</p>
       </span>
     </article>`
   )
-  console.log("Here is the card id: " + idea.id)
+  console.log("Here is the card id: " + cardData.id)
 }
 
-function stringify(idea) {
-  var stringifiedCard = JSON.stringify(idea);
+function stringify(ideaObj) {
+  var stringifiedCard = JSON.stringify(ideaObj);
   console.log("Make sure this thing is stringified: " + stringifiedCard);
-  localStorage.setItem(idea.id, stringifiedCard)
+  localStorage.setItem('uid', stringifiedCard);
+
+}
+
+function getData() {
+  var ideaObject = localStorage.getItem('uid');
+  console.log("The retrieved data is " + ideaObject);
+  createCardHtml(ideaObject);
 }
 
 function clearInputs() {
